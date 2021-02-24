3751110d2a75b09e60fc890d242fb80b40dd61e0
Zach Parkes <zach@sprigghr.com>
2021-02-24 13:31:22 -0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # 'pages' is interpreted as pages_controller.rb
 
 Rails.application.routes.draw do
+
   root 'pages#home' # pages controller, home action
+
+  get 'about', to: 'pages#about' # GET request to About page - sent to controller#action 
+
 end
